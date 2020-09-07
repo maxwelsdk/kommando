eacc915303dadbba3eb61a7c5ff8de2b7227631b
maxwelsdk <maxwel.sdk@hotmail.com>
2020-09-07 04:02:18 -0300

    Refactor

--- lib/authentication/data/i_firebase_auth.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract class IFirebaseAuth {
-  Future<UserCredential> createUserWithEmailAndPassword(String email, String password);
-  Future<UserCredential> loginWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-}
